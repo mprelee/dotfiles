07b8526af687ee2e1d5a89a6e6b108d4bb04467b
mprelee <mprelee@gmail.com>
2018-03-07 21:30:44 -0600

    Fixes cask apps.

--- osx_bootstrap.sh
+++ osx_bootstrap.sh
@@ -33,15 +33,14 @@ fi
 
 # Update homebrew recipes
 brew update
+brew upgrade
 
 # Install GNU core utilities (those that come with OS X are outdated)
-brew tap homebrew/dupes
 brew install coreutils
 brew install gnu-sed --with-default-names
 brew install gnu-tar --with-default-names
 brew install gnu-indent --with-default-names
 brew install gnu-which --with-default-names
-brew install gnu-grep --with-default-names
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
 brew install findutils
@@ -92,39 +91,26 @@ echo "Cleaning up..."
 brew cleanup
 
 echo "Installing cask..."
-brew install caskroom/cask/brew-cask
+brew tap caskroom/cask
 
 CASKS=(
-    colluquy
-    dropbox
-    firefox
-    flux
+    1password
+    atom
     google-chrome
-    google-drive
-    gpgtools
     iterm2
     macvim
     moom
-    skype
-    slack
-    spectacle
-    vagrant
-    virtualbox
     vlc
     xquartz
 )
 
 echo "Installing cask apps..."
-brew cask install ${CASKS[@]}
 
-#echo "Installing fonts..."
-#brew tap caskroom/fonts
-#FONTS=(
-#    font-inconsolidata
-#    font-roboto
-#    font-clear-sans
-#)
-#brew cask install ${FONTS[@]}
+for i in "${CASKS[@]}"
+do
+    brew cask install $i
+done
+brew cask cleanup
 
 echo "Installing Python packages..."
 PYTHON_PACKAGES=(
@@ -134,21 +120,10 @@ PYTHON_PACKAGES=(
 )
 sudo pip install ${PYTHON_PACKAGES[@]}
 
-#echo "Installing Ruby gems"
-#RUBY_GEMS=(
-#    bundler
-#    filewatcher
-#    cocoapods
-#)
-#sudo gem install ${RUBY_GEMS[@]}
-
 echo "Installing global npm packages..."
 npm install marked -g
 
 echo "Configuring OSX..."
-
-# Set fast key repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 0
 
 # Require password as soon as screensaver or sleep mode starts
 defaults write com.apple.screensaver askForPassword -int 1
@@ -163,9 +138,5 @@ defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 # Disable "natural" scroll
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-
-echo "Creating folder structure..."
-[[ ! -d Wiki ]] && mkdir Wiki
-[[ ! -d Workspace ]] && mkdir Workspace
 
 echo "Bootstrapping complete"
